226d83a2fbe328716fb8bf32df7b5f4f298f2e7f
Duda Sena <duda.sruiz@gmail.com>
2025-06-04 06:50:12 -0300

    Base

---
+++ src/No.java
@@ -0,0 +1,6 @@
+public class No {                                  //cada nó da árvore
+    private int valor;                             //valor guardado em cada nó
+    private No esquerda;                           //apontamento esquerdo e debaixo o direito.
+    private No direita;                            //automaticamente já vão ser nulos de início, quando nó recebe valor.
+}
+
